f57e726437dea725e43322e64006dd7d02403445
ray-anuska <91963539+ray-anuska@users.noreply.github.com>
2022-05-08 01:54:24 +0530

    Update Attendance.py

--- Attendance.py
+++ Attendance.py
@@ -70,7 +70,7 @@ def attendance(cap):
                 print(name)
                 markAttendance(name)
                 y1, x2, y2, x1 = faceLoc
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 20)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
         cv2.imshow('Webcam', img)
