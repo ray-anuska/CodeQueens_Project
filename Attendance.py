a342d5c79833ccacdd9bb6102fe15401724bf1c2
ray-anuska <91963539+ray-anuska@users.noreply.github.com>
2022-05-07 12:16:46 +0530

    Update Attendance.py

--- Attendance.py
+++ Attendance.py
@@ -69,14 +69,12 @@ while True:
             print(name)
             y1, x2, y2, x1 = faceLoc
             #y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 0), 20)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 20)
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
         
     cv2.imshow('Webcam', img)
-    #cv2.waitKey(1)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27: 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
     
 cap.release()
